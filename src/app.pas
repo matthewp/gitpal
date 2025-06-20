8b390536289c6690d04d901e0aeac251a85c59f7
Matthew Phillips <matthew@skypack.dev>
2025-06-20 08:18:46 -0400

    working version with fullscreen

--- src/app.pas
+++ src/app.pas
@@ -14,6 +14,7 @@ type
   TSelectModel = class(bobaui.TModel)
   private
     FTerminalWidth, FTerminalHeight: integer;
+    FSelectedIndex: integer;
   public
     constructor Create; overload;
     constructor Create(AWidth, AHeight: integer); overload;
@@ -27,6 +28,7 @@ begin
   inherited Create;
   FTerminalWidth := 80;
   FTerminalHeight := 24;
+  FSelectedIndex := 0;
 end;
 
 constructor TSelectModel.Create(AWidth, AHeight: integer);
@@ -34,6 +36,7 @@ begin
   inherited Create;
   FTerminalWidth := AWidth;
   FTerminalHeight := AHeight;
+  FSelectedIndex := 0;
 end;
 
 destructor TSelectModel.Destroy;
@@ -42,8 +45,23 @@ begin
 end;
 
 function TSelectModel.View: string;
+var
+  Options: array[0..1] of AnsiString;
+  I: integer;
 begin
-  Result := 'Hello world';
+  Options[0] := AnsiString('Accept');
+  Options[1] := AnsiString('Decline');
+  
+  Result := AnsiString('Commit changes?') + #10 + #10;
+  
+  for I := 0 to 1 do
+  begin
+    if I = FSelectedIndex then
+      Result := Result + AnsiString('> ')
+    else
+      Result := Result + AnsiString('  ');
+    Result := Result + Options[I] + #10;
+  end;
 end;
 
 function TSelectModel.Update(const Msg: bobaui.TMsg): bobaui.TUpdateResult;
@@ -63,6 +81,27 @@ begin
     if (KeyMsg.Key = 'q') or (KeyMsg.Key = 'Q') then
     begin
       Result.Cmd := bobaui.QuitCmd;
+    end
+    // Handle up arrow or 'k' to move up
+    else if KeyMsg.IsUpArrow or (KeyMsg.Key = 'k') or (KeyMsg.Key = 'K') then
+    begin
+      if FSelectedIndex > 0 then
+        FSelectedIndex := FSelectedIndex - 1;
+    end
+    // Handle down arrow or 'j' to move down
+    else if KeyMsg.IsDownArrow or (KeyMsg.Key = 'j') or (KeyMsg.Key = 'J') then
+    begin
+      if FSelectedIndex < 1 then
+        FSelectedIndex := FSelectedIndex + 1;
+    end
+    // Handle Enter to select option
+    else if KeyMsg.Key = #13 then
+    begin
+      if FSelectedIndex = 0 then
+        writeln('Accept selected')
+      else
+        writeln('Decline selected');
+      Result.Cmd := bobaui.QuitCmd;
     end;
   end
   else if Msg is bobaui.TWindowSizeMsg then
@@ -71,6 +110,7 @@ begin
     if (WindowMsg.Width <> FTerminalWidth) or (WindowMsg.Height <> FTerminalHeight) then
     begin
       NewModel := TSelectModel.Create(WindowMsg.Width, WindowMsg.Height);
+      NewModel.FSelectedIndex := FSelectedIndex;
       Result.Model := NewModel;
     end;
   end;
@@ -82,7 +122,7 @@ var
   Display: bobaui.TDisplay;
 begin
   Model := TSelectModel.Create(80, 24);
-  Prog := TBobaUIProgram.Create(Model, bobaui.dmInline);
+  Prog := TBobaUIProgram.Create(Model, bobaui.dmFullscreen);
   
   // Set global program reference
   GlobalProgram := Pointer(Prog);
