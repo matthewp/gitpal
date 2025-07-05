aacbcc71392f752da2f637b14cbb24537baadad6
Matthew Phillips <matthew@skypack.dev>
2025-07-05 10:37:09 -0400

    0.2.0

--- src/gitpal.pas
+++ src/gitpal.pas
@@ -13,7 +13,7 @@ uses
   config_manager;
 
 const
-  AppVersion = '0.1.0';  // Update this when tagging new releases
+  AppVersion = '0.2.0';  // Update this when tagging new releases
 
 function EnsureConfigurationExists: Boolean;
 var
